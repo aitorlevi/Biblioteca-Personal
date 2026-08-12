49919c298e2e8711618aaff1f5baec2fecba6529
Aitor Ledesma <aitorlevi@gmail.com>
2026-08-12 21:48:27 +0200

    FEAT - Update book info page layout and styles

--- src/modules/render.js
+++ src/modules/render.js
@@ -1,3 +1,5 @@
+const DEFAULT_BOOK_COVER = "/public/images/image-not-found.png";
+
 export function printBooks(books) {
     console.log(books);
     const container = document.querySelector("#searchResults");
@@ -37,33 +39,47 @@ export function printBookInfo(book) {
     const container = document.querySelector("#bookInfo");
 
     const bookData = book.volumeInfo;
+    const title = book.volumeInfo.title;
+    const subtitle = book.volumeInfo.subtitle ?? null;
     const authors =
-        bookData.authors && bookData.authors.length > 0
-            ? bookData.authors.join()
+        book.volumeInfo.authors && book.volumeInfo.authors.length > 0
+            ? book.volumeInfo.authors.join()
             : "Autor desconocido";
-    const categories =
-        bookData.categories && bookData.categories.length > 0
-            ? bookData.categories.join()
-            : "";
-    const picture =
-        bookData.imageLinks && bookData.imageLinks.thumbnail
-            ? bookData.imageLinks.thumbnail
-            : "../../../../public/images/image-not-found.png";
     const price = `${book.saleInfo.listPrice.amount} ${book.saleInfo.listPrice.currencyCode === "EUR" ? "€" : " - moneda desconocida"}`;
+    const isbn =
+        bookData.industryIdentifiers[1].identifier ??
+        bookData.industryIdentifiers[0].identifier;
+    const publishedDate = book.volumeInfo.publishedDate;
+    const publisher = book.volumeInfo.publisher;
+    const pageCount = book.volumeInfo.pageCount;
+    const categories =
+        book.volumeInfo.categories && book.volumeInfo.categories.length > 0
+            ? book.volumeInfo.categories.join()
+            : "";
+    const description = book.volumeInfo.description;
+
+    const pageTitle = document.querySelector("#pageTitle");
+    pageTitle.innerHTML = `AitorTeca - ${title}`;
 
     container.innerHTML = `<div class="book-info__main">
                         <div class="book-info__image-container">
-                            <img
-                                class="img book-info__image"
-                                src="${picture}"
-                                alt="${bookData.title}"
-                            />
+                            ${
+                                bookData.imageLinks
+                                    ? `<picture>
+                                    <source media="(min-width: 1200px)" srcset="${bookData.imageLinks.large}"></source>
+                                    <source media="(min-width: 768px)" srcset="${bookData.imageLinks.medium}"></source>
+                                    <img class="img book-info__image" src="${bookData.imageLinks.small}" alt="${bookData.title} loading="eager"></img>
+                                </picture>`
+                                    : "<img class='img book-info__image' src='../../../../public/images/image-not-found.png' alt='Imagen no encontrada' />"
+                            }
+                            
                         </div>
                         <div class="book-info__titles">
                             <div class="book-info__copy">
-                                <h2>${bookData.title}</h2>
-                                ${bookData.subtitle ? `<h4>${bookData.subtitle}</h4>` : ""}
-                                <h5>${authors}</h5>
+                                <h2>${title}</h2>
+                                ${subtitle ? `<h4>${subtitle}</h4>` : ""}
+                                <h4>${authors}</h4>
+                                <h5>Precio aproximado en librerías: ${price}</h5>
                             </div>
                             <div class="book-info__buttons">
                                 <button class="btn btn__primary">
@@ -75,18 +91,17 @@ export function printBookInfo(book) {
                     <div class="book__data">
                         <h3 class="book__data-title">DATOS DEL LIBRO</h3>
                         <ul class="book__data-list">
-                            <li class="book__data-item">Fecha de publicación: ${bookData.publishedDate}</li>
-                            <li class="book__data-item">Editorial: ${bookData.publisher}</li>
-                            <li class="book__data-item">Géneros: ${categories}</li>
-                            <li class="book__data-item">Nº de páginas: ${bookData.pageCount}</li>
-                            <li class="book__data-item">ISBN: ${bookData.industryIdentifiers.length > 1 ? bookData.industryIdentifiers[1].identifier : bookData.industryIdentifiers[0].identifier}</li>
-                            <li class="book__data-item">Precio: ${price}</li>
+                            <li class="book__data-item"><b>ISBN</b>: ${isbn}</li>
+                            <li class="book__data-item"><b>Fecha de publicación</b>: ${publishedDate}</li>
+                            <li class="book__data-item"><b>Editorial</b>: ${publisher}</li>
+                            <li class="book__data-item"><b>Géneros</b>: ${categories}</li>
+                            <li class="book__data-item"><b>Número de páginas</b>: ${pageCount}</li>
                         </ul>
                     </div>
                     <div class="book__summary">
                         <h3>SINOPSIS</h3>
                         <div class="book__summary-copy">
-                            ${bookData.description}
+                            ${description}
                         </div>
                     </div>`;
 }
