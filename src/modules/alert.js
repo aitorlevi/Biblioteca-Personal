30c720436a40c673c138bbef149a7f068ef8a0d5
Aitor Ledesma <aitorlevi@gmail.com>
2026-08-23 19:38:33 +0200

    FEAT - Add alert message functionality for status updates and removals

--- src/modules/alert.js
+++ src/modules/alert.js
@@ -1,15 +1,18 @@
 const alertMessage = document.querySelector("#alertMessage");
 
-export function showAlert(type, message) {
+export function showAlert(type, messageDescription) {
+    let message = null;
     switch (type) {
         case "success":
             alertMessage.classList.add("alert--success");
-
+            message = `<h4>¡Éxito!</h4><p>${messageDescription}</p>`;
             break;
         case "error":
             alertMessage.classList.add("alert--error");
+            message = `<h4>¡Error!</h4><p>${messageDescription}</p>`;
             break;
         default:
+            message = `<h4>!Info!</h4><p>${messageDescription}</p>`;
             break;
     }
     alertMessage.innerHTML = message;
