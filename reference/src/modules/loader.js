69c85fdd33af4c0ec1023637d3e3194585c1634f
Aitor Ledesma <aitorlevi@gmail.com>
2026-08-29 18:49:54 +0200

    CHORE - Untrack reference/

--- reference/src/modules/loader.js
+++
@@ -1,9 +0,0 @@
-const loader = document.querySelector("#loader");
-
-export function showLoader() {
-    loader.classList.add("show");
-}
-
-export function hideLoader() {
-    loader.classList.remove("show");
-}
